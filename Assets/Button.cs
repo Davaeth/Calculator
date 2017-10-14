3338452afcadaa20c0df39a4b0f1f28ece1108ba
Patryk Połeć <masterbelki@gmail.com>
2017-10-14 14:13:27 +0200

    Add files via upload

---
+++ Assets/Button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+
+	public void StartGame ()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Dodawanie()
+    {
+        SceneManager.LoadScene("Dodawanie");
+    }
+
+    public void Odejmowanie()
+    {
+        SceneManager.LoadScene("Odejmowanie");
+    }
+
+    public void Mnozenie()
+    {
+        SceneManager.LoadScene("Mnozenie");
+    }
+
+    public void Dzielenie()
+    {
+        SceneManager.LoadScene("Dzielenie");
+    }
+}
---
+++ Assets/Działanie.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Działanie : MonoBehaviour {
+
+    public InputField input1;
+    public InputField input2;
+
+    public Text hasil;
+
+    public void Dodawanie()
+    {
+        int wynik;
+
+        wynik = int.Parse(input1.text) + int.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Odejmowanie()
+    {
+        int wynik;
+
+        wynik = int.Parse(input1.text) - int.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Mnozenie()
+    {
+        double wynik;
+
+        wynik = double.Parse(input1.text) * double.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Dzielenie()
+    {
+        double wynik;
+
+        wynik = double.Parse(input1.text) / double.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+}
