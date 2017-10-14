650468f7305f41ff8101724ee8a86a3ccce550cc
Patryk Połeć <masterbelki@gmail.com>
2017-10-14 15:00:21 +0200

    New option - Pow

---
+++ Assets/Projects/Button.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene("Main");
+    }
+
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Dodawanie()
+    {
+        SceneManager.LoadScene("Dodawanie");
+    }
+
+    public void Odejmowanie()
+    {
+        SceneManager.LoadScene("Odejmowanie");
+    }
+
+    public void Mnozenie()
+    {
+        SceneManager.LoadScene("Mnozenie");
+    }
+
+    public void Dzielenie()
+    {
+        SceneManager.LoadScene("Dzielenie");
+    }
+
+    public void Potegowanie()
+    {
+        SceneManager.LoadScene("Potegowanie");
+    }
+}
---
+++ Assets/Projects/Działanie.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Działanie : MonoBehaviour {
+
+    public InputField input1;
+    public InputField input2;
+
+    public Text hasil;
+
+    public void Dodawanie()
+    {
+        int wynik;
+
+        wynik = int.Parse(input1.text) + int.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Odejmowanie()
+    {
+        int wynik;
+
+        wynik = int.Parse(input1.text) - int.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Mnozenie()
+    {
+        double wynik;
+
+        wynik = double.Parse(input1.text) * double.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Dzielenie()
+    {
+        double wynik;
+
+        wynik = double.Parse(input1.text) / double.Parse(input2.text);
+
+        hasil.text = wynik.ToString();
+    }
+
+    public void Potega()
+    {
+        double wynik;
+
+        wynik = Mathf.Pow(float.Parse(input1.text),float.Parse(input2.text));
+
+        hasil.text = wynik.ToString();
+    }
+}
